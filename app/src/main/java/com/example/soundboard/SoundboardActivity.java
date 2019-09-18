f3918a3217307d980475a73e712a57f77a197748
Isaacblekhman2003 <isaacblekhman14@gmail.com>
2019-09-18 13:59:22 -0700

    F

--- app/src/main/java/com/example/soundboard/SoundboardActivity.java
+++ app/src/main/java/com/example/soundboard/SoundboardActivity.java
@@ -13,24 +13,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class SoundboardActivity extends AppCompatActivity implements OnTouchListener  {
+public class SoundboardActivity extends AppCompatActivity {
     private SoundPool soundPool;
     private int soundID;
     private Button bruh;
     private Button swamp;
     private Button darude;
     boolean loaded = false;
+    private boolean isloaded;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Button[] buttons = new Button[12];
 
-        wireWidgets();
-        bruh.setOnTouchListener(this);
-        // Set the hardware buttons to control the music
+        setContentView(R.layout.activity_main);
+        bruh = findViewById(R.id.button_main_bruh);
+        swamp = findViewById(R.id.button_main_myswomp);
+        darude = findViewById(R.id.button_main_darude);
+        listeners();
         this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
-        // Load the sound
         soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
         soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
             @Override
@@ -40,32 +43,36 @@ public class SoundboardActivity extends AppCompatActivity implements OnTouchList
             }
         });
         soundID = soundPool.load(this, R.raw.darude, 1);
+    }
+    private void listeners() {
+        bruh.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                                        float actualVolume = (float) audioManager
+                                                .getStreamVolume(AudioManager.STREAM_MUSIC);
+                                        float maxVolume = (float) audioManager
+                                                .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                                        float volume = actualVolume / maxVolume;
+                                        // Is the sound loaded already?
+                                        if (loaded) {
+                                            soundPool.play(soundID, volume, volume, 1, 0, 1f);
+                                            Log.e("Test", "Played sound");
+                                        }
+                                    }
+
+
+                                    });
+                                }
+
 
     }
 
-    private void wireWidgets() {
-        bruh = findViewById(R.id.button_main_bruh);
-        swamp = findViewById(R.id.button_main_myswomp);
-        darude = findViewById(R.id.button_main_darude);
-    }
 
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            // Getting the user sound settings
-            AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-            float actualVolume = (float) audioManager
-                    .getStreamVolume(AudioManager.STREAM_MUSIC);
-            float maxVolume = (float) audioManager
-                    .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-            float volume = actualVolume / maxVolume;
-            // Is the sound loaded already?
-            if (loaded) {
-                soundPool.play(soundID, volume, volume, 1, 0, 1f);
-                Log.e("Test", "Played sound");
-            }
-        }
-        return false;
-    }
-}
+
+
+
+
+
+
 
