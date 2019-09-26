78e6cb7ec2592de03305a22b2fb2af06472ed70c
Isaacblekhman2003 <isaacblekhman14@gmail.com>
2019-09-26 13:55:52 -0700

    chad

--- app/src/main/java/com/example/soundboard/Song.java
+++ app/src/main/java/com/example/soundboard/Song.java
@@ -1,4 +1,61 @@
 package com.example.soundboard;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Song {
+    private List<Note> song;
+
+
+    public Song(Note[] songs) {
+        song = Arrays.asList(songs);
+        song = new ArrayList<>();
+
+    }
+
+    public Song() {
+        song = new ArrayList<>();
+    }
+
+    public void addNote(Note note) {
+        song.add(note);
+    }
+
+    public void removeNote(int location)
+    {
+        song.remove(location);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
